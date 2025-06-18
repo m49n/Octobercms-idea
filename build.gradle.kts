d374132295fde1d100850665177d30f4b5d92c50
WebWinsae <akpomah@gmail.com>
2025-06-18 13:31:40 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    // разворачиваем Java-проект
+    id("java")
+    // плагин IntelliJ Platform
+    id("org.jetbrains.intellij") version "2.5.0"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    // версия IDE-SDK, в котором вы тестируете плагин
+    version.set("2021.3")
+    // подтягиваем поддержку PHP-плагина
+    plugins.set(listOf("com.jetbrains.php"))
+    // тип IDE: IC = Community, IU = Ultimate
+    type.set("IC")
+}
+
+java {
+    // используем toolchain для установки Java 17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks {
+    // если хочется ещё явную совместимость — но toolchain обычно достаточно
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+}
